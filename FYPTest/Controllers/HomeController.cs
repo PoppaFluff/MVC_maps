7bb2df57822eed611a44340df35962442f7a87d1
David Renton <davidleerenton@gmail.com>
2016-02-22 16:44:41 +0000

    Maps now working, going to start the clustering

--- FYPTest/Controllers/HomeController.cs
+++ FYPTest/Controllers/HomeController.cs
@@ -34,17 +34,48 @@ namespace FYPTest.Controllers
 
             return View(db.testDBs.ToList());
         }
-        [HttpPost]
-        public ActionResult Dropdown(string name)
+       
+        public JsonResult Drop(string name)
         {
+            List<testDB> people = new List<testDB>();
             var query = from a in db.testDBs
-                        where a.Surname.Contains(name)
+                        where a.County.Contains(name)
                         select a;
-            var item = query.FirstOrDefault();
-            if (item != null)
-                return View(query.ToList());
-            else
-                return View("NotFound");  //Let's show a view about item not found
+            //var item = query.FirstOrDefault();
+            //if (item != null){
+                foreach(var q in query){
+                    people.Add(new testDB
+                    {
+                        Surname = q.Surname,
+                        Forename = q.Forename,
+                        TownlandStreet = q.TownlandStreet,
+                        DED = q.DED,
+                        County = q.County,
+                        Age = q.Age,
+                        Sex = q.Sex,
+                        Birthplace = q.Birthplace,
+                        Occupation = q.Occupation,
+                        Religion = q.Religion,
+                        Literacy = q.Literacy,
+                        IrishLanguage = q.IrishLanguage,
+                        RelationToHeadOfHousehold = q.RelationToHeadOfHousehold,
+                        MaritalStatus = q.MaritalStatus,
+                        SpecifiedIllness = q.SpecifiedIllness,
+                        Religion_ID = q.Religion_ID,
+                        Lat = q.Lat,
+                        Long = q.Long,
+                        AddressToGeoCode = q.AddressToGeoCode,
+                        HouseNo = q.HouseNo,
+                        AmountInHouse = q.AmountInHouse,
+                        OriginalCensusForm = q.OriginalCensusForm,
+                        LocationType = q.LocationType
+                    });
+
+                }
+                return Json(people, JsonRequestBehavior.AllowGet);
+            //}
+            //else
+            //    return View("NotFound");  //Let's show a view about item not found
         }
     }
 }
--- FYPTest/Scripts/David.js
+++ FYPTest/Scripts/David.js
@@ -20,12 +20,12 @@ function Initialize() {
 
     // Google has tweaked their interface somewhat - this tells the api to use that new UI
    // google.maps.visualRefresh = true;
-    var Liverpool = new google.maps.LatLng(53.408841, -2.981397);
+    var Athlone = new google.maps.LatLng(53.4232693, -7.9490146);
 
     // These are options that set initial zoom level, where the map is centered globally to start, and the type of map to show
     var mapOptions = {
-        zoom: 14,
-        center: Liverpool,
+        zoom: 8,
+        center: Athlone,
         mapTypeId: google.maps.MapTypeId.G_NORMAL_MAP
     };
 
@@ -44,35 +44,80 @@ function Initialize() {
     // You can make markers different colors...  google it up!
     marker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png')
 
-    // a sample list of JSON encoded data of places to visit in Liverpool, UK
-    // you can either make up a JSON list server side, or call it from a controller using JSONResult
-    var data = [
-              { "Id": 1, "PlaceName": "Liverpool Museum", "OpeningHours": "9-5, M-F", "GeoLong": "53.410146", "GeoLat": "-2.979919" },
-              { "Id": 2, "PlaceName": "Merseyside Maritime Museum ", "OpeningHours": "9-1,2-5, M-F", "GeoLong": "53.401217", "GeoLat": "-2.993052" },
-              { "Id": 3, "PlaceName": "Walker Art Gallery", "OpeningHours": "9-7, M-F", "GeoLong": "53.409839", "GeoLat": "-2.979447" },
-              { "Id": 4, "PlaceName": "National Conservation Centre", "OpeningHours": "10-6, M-F", "GeoLong": "53.407511", "GeoLat": "-2.984683" }
-    ];
+    //// a sample list of JSON encoded data of places to visit in Liverpool, UK
+    //// you can either make up a JSON list server side, or call it from a controller using JSONResult
+    //var data = [
+    //          { "Id": 1, "PlaceName": "Liverpool Museum", "OpeningHours": "9-5, M-F", "GeoLong": "53.410146", "GeoLat": "-2.979919" },
+    //          { "Id": 2, "PlaceName": "Merseyside Maritime Museum ", "OpeningHours": "9-1,2-5, M-F", "GeoLong": "53.401217", "GeoLat": "-2.993052" },
+    //          { "Id": 3, "PlaceName": "Walker Art Gallery", "OpeningHours": "9-7, M-F", "GeoLong": "53.409839", "GeoLat": "-2.979447" },
+    //          { "Id": 4, "PlaceName": "National Conservation Centre", "OpeningHours": "10-6, M-F", "GeoLong": "53.407511", "GeoLat": "-2.984683" }
+    //];
 
-    // Using the JQuery "each" selector to iterate through the JSON list and drop marker pins
-    $.each(data, function (i, item) {
-        var marker = new google.maps.Marker({
-            'position': new google.maps.LatLng(item.GeoLong, item.GeoLat),
-            'map': map,
-            'title': item.PlaceName
-        });
+    //// Using the JQuery "each" selector to iterate through the JSON list and drop marker pins
+    //$.each(data, function (i, item) {
+    //    var marker = new google.maps.Marker({
+    //        'position': new google.maps.LatLng(item.GeoLong, item.GeoLat),
+    //        'map': map,
+    //        'title': item.PlaceName
+    //    });
 
-        // Make the marker-pin blue!
-        marker.setIcon('http://maps.google.com/mapfiles/ms/icons/blue-dot.png')
+    //    // Make the marker-pin blue!
+    //    marker.setIcon('http://maps.google.com/mapfiles/ms/icons/blue-dot.png')
 
-        // put in some information about each json object - in this case, the opening hours.
-        var infowindow = new google.maps.InfoWindow({
-            content: "<div class='infoDiv'><h2>" + item.PlaceName + "</h2>" + "<div><h4>Opening hours: " + item.OpeningHours + "</h4></div></div>"
-        });
+    //    // put in some information about each json object - in this case, the opening hours.
+    //    var infowindow = new google.maps.InfoWindow({
+    //        content: "<div class='infoDiv'><h2>" + item.PlaceName + "</h2>" + "<div><h4>Opening hours: " + item.OpeningHours + "</h4></div></div>"
+    //    });
 
-        // finally hook up an "OnClick" listener to the map so it pops up out info-window when the marker-pin is clicked!
-        google.maps.event.addListener(marker, 'click', function () {
-            infowindow.open(map, marker);
-        });
+    //    // finally hook up an "OnClick" listener to the map so it pops up out info-window when the marker-pin is clicked!
+    //    google.maps.event.addListener(marker, 'click', function () {
+    //        infowindow.open(map, marker);
+    //    });
 
+    //})
+}
+function PopulateMap() {
+    var Athlone = new google.maps.LatLng(53.4232693, -7.9490146);
+
+    // These are options that set initial zoom level, where the map is centered globally to start, and the type of map to show
+    var mapOptions = {
+        zoom: 8,
+        center: Athlone,
+        mapTypeId: google.maps.MapTypeId.G_NORMAL_MAP
+    };
+
+    // This makes the div with id "map_canvas" a google map
+    var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    var n = document.getElementById("textSearch").value;
+    alert(n);
+    $.ajax({
+        type: 'POST',
+        url: '/Home/Drop',
+        data: { name: n },
+        success: function (data) {
+            alert("success!!!");
+            var items = '';
+            $.each(data, function (i, item) {
+                alert(item.Lat + "Bollocks");
+                var marker = new google.maps.Marker({
+                    'position': new google.maps.LatLng(item.Lat, item.Long),
+                    'map': map,
+                    'title': item.AddressToGeocode
+                });
+
+                // Make the marker-pin blue!
+                marker.setIcon('http://maps.google.com/mapfiles/ms/icons/blue-dot.png')
+
+                // put in some information about each json object - in this case, the opening hours.
+                var infowindow = new google.maps.InfoWindow({
+                    content: "<div><h2>" + item.RelationToHeadOfHousehold + "</h2>" + "<div><h4>Occupant: " + item.Forename + " " + item.Surname + "</h4></div></div>"
+                });
+
+                // finally hook up an "OnClick" listener to the map so it pops up out info-window when the marker-pin is clicked!
+                google.maps.event.addListener(marker, 'click', function () {
+                    infowindow.open(map, marker);
+                });
+            })
+        }
     })
 }
